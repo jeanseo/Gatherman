4ac7bdb85721fac58412180eb59456695a3f2594
Jean-Sébastien Merz <jsmpub@gmail.com>
2019-05-24 17:40:48 +0200

    Master view préparée

--- Gatherman/Gatherman/Views/MasterPage.xaml.cs
+++ Gatherman/Gatherman/Views/MasterPage.xaml.cs
@@ -12,9 +12,17 @@ namespace Gatherman.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MasterPage : ContentPage
 	{
-		public MasterPage ()
+        public Models.User loggedUser;
+
+        public MasterPage ()
 		{
 			InitializeComponent ();
+
 		}
-	}
+
+        protected override async void OnAppearing()
+        {
+            var picture = ImageSource.FromResource("Gatherman.images.default_portrait.png");
+        }
+    }
 }
